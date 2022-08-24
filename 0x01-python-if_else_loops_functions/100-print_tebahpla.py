7af3da76e0ddca05dbf790c965a016e1cddb1f07
Tayo Adebusuyi <adebusuyitayo@gmail.com>
2022-08-24 09:49:53 +0000

    loop

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-i = 122
-while i >= 97:
-    flag = 0
-    if i % 2 != 0:
-        i = i - 32
-        flag = 1
-    print("{:s}".format(chr(i)), end="")
-    if flag == 1:
-        i = i + 32
-    i = i - 1
+for letter in range(122, 96, -1):
+    if letter % 2 != 0:
+        print("{:c}".format(letter - 32), end="")
+    else:
+        print("{:c}".format(letter), end="")
